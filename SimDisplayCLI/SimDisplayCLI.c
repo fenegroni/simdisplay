a42679ccad6d2255a6bb1053d021275c1a63c4c7
Filippo Negroni <f.e.negroni@gmail.com>
2020-01-17 00:39:49 +0000

    Improve error handling for mem mapped files
    We now handle the case where files opened correctly but didn't actually
    exist.
    This is an issue because we would just be reading garbage anyway at this
    point.
    But, having said that, maybe we should wait until the app runs and then
    we can open those files...
    
    Ok so we should just enter a loop where we wait for the files to open.

--- SimDisplayCLI/SimDisplayCLI.c
+++ SimDisplayCLI/SimDisplayCLI.c
@@ -7,50 +7,63 @@
 
 int main(void)
 {
+    int err = 0;
+
     HANDLE phyMap = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, sizeof(struct ACCPhysics), TEXT("Local\\acpmf_physics"));
     if (!phyMap) {
-        fprintf(stderr, "Error create file mapping for ACCPhysics.\n");
+        fprintf(stderr, "Error: create file mapping for ACCPhysics.\n");
+        err = 1;
+    } else if (ERROR_ALREADY_EXISTS != GetLastError()) {
+        fprintf(stderr, "Error: ACCPhysics does not exist.\n");
+        err = 2;
     }
     HANDLE graMap = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, sizeof(struct ACCGraphics), TEXT("Local\\acpmf_graphics"));
     if (!graMap) {
-        fprintf(stderr, "Error create file mapping for ACCGraphics.\n");
+        fprintf(stderr, "Error: create file mapping for ACCGraphics.\n");
+        err = 1;
+    } else if (ERROR_ALREADY_EXISTS != GetLastError()) {
+        fprintf(stderr, "Error: ACCGraphics does not exist.\n");
+        err = 2;
     }
     HANDLE staMap = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, sizeof(struct ACCStatic), TEXT("Local\\acpmf_static"));
     if (!staMap) {
-        fprintf(stderr, "Error create file mapping for ACCStatic.\n");
+        fprintf(stderr, "Error: create file mapping for ACCStatic.\n");
+        err = 1;
+    } else if (ERROR_ALREADY_EXISTS != GetLastError()) {
+        fprintf(stderr, "Error: ACCStatic does not exist.\n");
+        err = 2;
     }
-    if (!staMap || !graMap || !phyMap) {
-        fprintf(stderr, "Exiting with code 1\n");
-        return 1;
+    if (err) {
+        fprintf(stderr, "Exiting with code %d\n", err);
+        return err;
     }
 
     struct ACCPhysics* phy = (struct ACCPhysics*) MapViewOfFile(phyMap, FILE_MAP_READ, 0, 0, 0);
     if (!phy) {
-        fprintf(stderr, "Error mapping view ACCPhysics.\n");
+        fprintf(stderr, "Error: mapping view ACCPhysics.\n");
+        err = 3;
     }
     struct ACCGraphics* gra = (struct ACCGraphics*) MapViewOfFile(graMap, FILE_MAP_READ, 0, 0, 0);
     if (!gra) {
-        fprintf(stderr, "Error mapping view ACCGraphics.\n");
+        fprintf(stderr, "Error: mapping view ACCGraphics.\n");
+        err = 3;
     }
     struct ACCStatic* sta = (struct ACCStatic*) MapViewOfFile(staMap, FILE_MAP_READ, 0, 0, 0);
     if (!sta) {
-        fprintf(stderr, "Error mapping view ACCStatic.\n");
+        err = 3;
+        fprintf(stderr, "Error: mapping view ACCStatic.\n");
+    }
+    if (err) {
+        fprintf(stderr, "Exiting with code %d\n", err);
+        return err;
     }
 
-    if (!phy || !gra || !sta) {
-        fprintf(stderr, "Exiting with code 2\n");
-        return 2;
+    HANDLE comPort = CreateFile(L"\\.\\COM3", GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+    if (comPort == INVALID_HANDLE_VALUE) {
+        fprintf(stderr, "Error: open serial port \\.\\COM3\n");
+        fprintf(stderr, "Exiting with code 10\n");
+        return 10;
     }
 
-    // Here we need to put the loop that writes to the serial port:
-    // each serial port communication is a start of comms marker, then a byte telling how many bytes follow,
-    // then 
-
-    /*
-    for (int i = 0; i < 500; ++i) {
-        fprintf(stdout, "abs = %f, tc = %f\n", phy->abs, phy->tc);
-        Sleep(500);
-    }
-    */
     return 0;
 }
