3118332056ede48fd595bf0efe2dcdb415c38e46
Filippo Negroni <f.e.negroni@gmail.com>
2020-01-23 23:41:55 +0000

    Read a record from file and copy to the three mapfile views
    We HeapAlloc the three strcutures as one buffer.
    Read one buffer at a time and CopyMemory from the buffers into
    the mapped views.
    
    I think I can do better: ReadFile directly into the three
    mapped views.

--- Windows/SimDisplayCLI/SimDisplayCLI.c
+++ Windows/SimDisplayCLI/SimDisplayCLI.c
@@ -167,53 +167,42 @@ doReplay(void)
 		fprintf(stderr, "Error create file mapping for ACCStatic.\n");
 	}
 	if (!staMap || !graMap || !phyMap) {
-		fprintf(stderr, "Exiting with code 1\n");
 		return 1;
 	}
 
-	struct ACCPhysics *phy = (struct ACCPhysics *) MapViewOfFile(phyMap, FILE_MAP_READ, 0, 0, 0);
-	if (!phy) {
+	struct ACCPhysics *phyView = (struct ACCPhysics *) MapViewOfFile(phyMap, FILE_MAP_READ, 0, 0, 0);
+	if (!phyView) {
 		fprintf(stderr, "Error mapping view ACCPhysics.\n");
 	}
-	struct ACCGraphics *gra = (struct ACCGraphics *) MapViewOfFile(graMap, FILE_MAP_READ, 0, 0, 0);
-	if (!gra) {
+	struct ACCGraphics *graView = (struct ACCGraphics *) MapViewOfFile(graMap, FILE_MAP_READ, 0, 0, 0);
+	if (!graView) {
 		fprintf(stderr, "Error mapping view ACCGraphics.\n");
 	}
-	struct ACCStatic *sta = (struct ACCStatic *) MapViewOfFile(staMap, FILE_MAP_READ, 0, 0, 0);
-	if (!sta) {
+	struct ACCStatic *staView = (struct ACCStatic *) MapViewOfFile(staMap, FILE_MAP_READ, 0, 0, 0);
+	if (!staView) {
 		fprintf(stderr, "Error mapping view ACCStatic.\n");
 	}
 
-	if (!phy || !gra || !sta) {
-		fprintf(stderr, "Exiting with code 2\n");
+	if (!phyView || !graView || !staView) {
 		return 2;
 	}
 
-	int maxCsvRecord = 8192;
-	char *csvRecord = malloc(maxCsvRecord);
-	if (!csvRecord) ExitProcess(1);
-	DWORD writtenBytes;
-	if (!WriteFile(csvFile, csvRecord,
-		snprintf(csvRecord, maxCsvRecord, "Status,Damage 0,Damage 1,Damage 2,Damage 3,Damage 4,PHY Pid,TC Active,ABS Active,TC,TC Cut,ABS,Lights Stage,Flashing Lights\n"),
-		&writtenBytes, NULL)) {
-		fprintf(stderr, "Error: write CSV header: %d\n", GetLastError());
-		return 3;
-	}
-	int binBufferSize = sizeof(struct ACCPhysics) + sizeof(struct ACCGraphics) + sizeof(struct ACCStatic);
-	char *binBuffer = malloc(binBufferSize);
-	if (!binBuffer) ExitProcess(1);
-	struct ACCPhysics *phy = (struct ACCPhysics *)binBuffer;
-	struct ACCGraphics *gra = (struct ACCGraphics *)(binBuffer + sizeof(struct ACCPhysics));
-	struct ACCStatic *sta = (struct ACCStatic *)(binBuffer + sizeof(struct ACCPhysics) + sizeof(struct ACCGraphics));
+	size_t binBufferSize = sizeof(struct ACCPhysics) + sizeof(struct ACCGraphics) + sizeof(struct ACCStatic);
+	char *binBuffer = HeapAlloc(GetProcessHeap(), HEAP_GENERATE_EXCEPTIONS, binBufferSize);
+	struct ACCPhysics *phyBin = (struct ACCPhysics *)binBuffer;
+	struct ACCGraphics *graBin = (struct ACCGraphics *)(binBuffer + sizeof(struct ACCPhysics));
+	struct ACCStatic *staBin = (struct ACCStatic *)(binBuffer + sizeof(struct ACCPhysics) + sizeof(struct ACCGraphics));
 	DWORD readBytes;
-	while (ReadFile(binFile, binBuffer, binBufferSize, &readBytes, NULL) && readBytes == binBufferSize) {
-		if (!WriteFile(csvFile, csvRecord,
-			snprintf(csvRecord, maxCsvRecord, "%d,%f,%f,%f,%f,%f,%f,%f,%d,%d,%d\n",
-				gra->status, phy->carDamage[0], phy->carDamage[1], phy->carDamage[2], phy->carDamage[3], phy->carDamage[4], phy->tc, phy->abs, gra->TC, gra->TCCut, gra->ABS),
-			&writtenBytes, NULL)) {
-			fprintf(stderr, "Error: write CSV record: %d\n", GetLastError());
-			return 4;
-		}
+	// every 20ms, read file and copy memory into structures...
+	// or read three times? we read slower so we can just read directly into the memory map?
+	while (ReadFile(binFile, binBuffer, (DWORD)binBufferSize, &readBytes, NULL) && readBytes == binBufferSize) {
+		// for each record in the bin file, copy memory to the three structures.
+#pragma warning (push)
+#pragma warning (disable:6387)
+		CopyMemory(phyView, phyBin, sizeof(struct ACCPhysics));
+		CopyMemory(graView, graBin, sizeof(struct ACCGraphics));
+		CopyMemory(staView, staBin, sizeof(struct ACCStatic));
+#pragma warning (pop)
 	}
 	return 0;
 }
