996a988a0e19aa9573be5542bde73edae5418543
Filippo Negroni <f.e.negroni@gmail.com>
2020-02-02 17:35:48 +0000

    Refactor mapAcpmf to allow Open or Create
    Got rid of typedef Errorcode following the linux kernel guidelines.
    mapAcpmf()  now allows Create or OpenExisting modes to ensure
    it can be reused in separate parts of the code.
    In SimDisplayPacket use types uint8_t and uint16_t to be explicit
    about memory.
    
    TODO: put SimDisplayPacket somewhere accessible to Arduino project.

--- Service/Windows/visualstudio/SimDisplayCLI/SimDisplayCLI.c
+++ Service/Windows/visualstudio/SimDisplayCLI/SimDisplayCLI.c
@@ -1,94 +1,106 @@
 #include <windows.h>
 #include <tchar.h>
 
-#include "..\ACCSharedMemory\ACCSharedMemory.h"
-
 #include <stdio.h>
 #include <math.h>
+#include <stdint.h>
 
-typedef enum {
-	ERR_NONE,
-	ERR_TIMER,
-	ERR_MAP,
-	ERR_COM,
-	ERR_FILE, ERR_FILE_WRITE, ERR_FILE_READ
-} Errorcode;
+#include "..\ACCSharedMemory\ACCSharedMemory.h"
 
-/* TODO: This method must be made more reusable so it can be used for reading and writing */
-Errorcode mapAcpmf(struct ACCPhysics **phy, struct ACCGraphics **gra, struct ACCStatic **sta)
+enum mapAcpmf_action {
+	MAPACPMF_CREATE,
+	MAPACPMF_OPEN_EXISTING
+};
+
+const wchar_t acpmf_physics[] = L"Local\\acpmf_physics";
+const wchar_t acpmf_graphics[] = L"Local\\acpmf_graphics";
+const wchar_t acpmf_static[] = L"Local\\acpmf_static";
+
+int mapAcpmf(enum mapAcpmf_action action, struct ACCPhysics **phy, struct ACCGraphics **gra, struct ACCStatic **sta)
 {
-	Errorcode err = ERR_NONE;
+	int err = 0;
 
-	HANDLE phyMap;
-	while (!(phyMap = OpenFileMapping(FILE_MAP_READ, FALSE, TEXT("Local\\acpmf_physics")))) {
-		fprintf(stderr, "Waiting: open file mapping for ACCPhysics.\n");
-		Sleep(1000);
+	HANDLE phyMap, graMap, staMap;
+
+	if (MAPACPMF_OPEN_EXISTING == action) {
+		while (!(phyMap = OpenFileMapping(FILE_MAP_READ, FALSE, acpmf_physics))) {
+			fprintf(stderr, "Waiting: open file mapping for ACCPhysics.\n");
+			Sleep(1000);
+			// TODO: don't try forever: exit after 5 minutes?
+		}
+		graMap = OpenFileMapping(FILE_MAP_READ, FALSE, acpmf_graphics); 
+		staMap = OpenFileMapping(FILE_MAP_READ, FALSE, acpmf_static);
+	} else {
+		phyMap = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, sizeof(struct ACCPhysics), acpmf_physics);
+		graMap = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, sizeof(struct ACCGraphics), acpmf_graphics);
+		staMap = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, sizeof(struct ACCStatic), acpmf_static);
 	}
-	HANDLE graMap = OpenFileMapping(FILE_MAP_READ, FALSE, TEXT("Local\\acpmf_graphics"));
+	if (!phyMap) {
+		fprintf(stderr, "Error: %d: file mapping ACCPhysics.\n", action);
+		err = 1;
+	}
 	if (!graMap) {
-		fprintf(stderr, "Error: open file mapping for ACCGraphics.\n");
-		err = ERR_MAP;
+		fprintf(stderr, "Error: %d: file mapping ACCGraphics.\n", action);
+		err = 1;
 	}
-	HANDLE staMap = OpenFileMapping(FILE_MAP_READ, FALSE, TEXT("Local\\acpmf_static"));
 	if (!staMap) {
-		fprintf(stderr, "Error: open file mapping for ACCStatic.\n");
-		err = ERR_MAP;
+		fprintf(stderr, "Error: %d: file mapping ACCStatic.\n", action);
+		err = 1;
 	}
 	if (err) {
 		return err;
 	}
 
-	*phy = (struct ACCPhysics *) MapViewOfFile(phyMap, FILE_MAP_READ, 0, 0, 0);
+	*phy = (struct ACCPhysics *) MapViewOfFile(phyMap, (action == MAPACPMF_OPEN_EXISTING) ? FILE_MAP_READ : FILE_MAP_WRITE, 0, 0, 0);
 	if (!phy) {
 		fprintf(stderr, "Error: mapping view ACCPhysics.\n");
-		err = ERR_MAP;
+		err = 1;
 	}
-	*gra = (struct ACCGraphics *) MapViewOfFile(graMap, FILE_MAP_READ, 0, 0, 0);
+	*gra = (struct ACCGraphics *) MapViewOfFile(graMap, (action == MAPACPMF_OPEN_EXISTING) ? FILE_MAP_READ : FILE_MAP_WRITE, 0, 0, 0);
 	if (!gra) {
 		fprintf(stderr, "Error: mapping view ACCGraphics.\n");
-		err = ERR_MAP;
+		err = 1;
 	}
-	*sta = (struct ACCStatic *) MapViewOfFile(staMap, FILE_MAP_READ, 0, 0, 0);
+	*sta = (struct ACCStatic *) MapViewOfFile(staMap, (action == MAPACPMF_OPEN_EXISTING) ? FILE_MAP_READ : FILE_MAP_WRITE, 0, 0, 0);
 	if (!sta) {
-		err = ERR_MAP;
+		err = 1;
 		fprintf(stderr, "Error: mapping view ACCStatic.\n");
 	}
 
 	return err;
 }
 
+//TODO: extract to shared header file so structs can be kept in sync!
 #pragma pack (push)
 #pragma pack (1)
 struct SimDisplayPacket {
-	unsigned char status, gear, tc, tcc, abs, bb, map, remlaps, airt, roadt;
-	short int rpms;
+	uint8_t status, gear, tc, tcc, abs, bb, map, remlaps, airt, roadt;
+	uint16_t rpms;
 };
 #pragma pack (pop)
 
 /* TODO: pass name of comport or list comports and scan for Arduino. */
-Errorcode doSend(void)
+int doSend(void)
 {
-	Errorcode err;
-
 	struct ACCPhysics *phy;
 	struct ACCGraphics *gra;
 	struct ACCStatic *sta;
 
-	if (err = mapAcpmf(&phy, &gra, &sta)) {
-		return err;
+	if (mapAcpmf(MAPACPMF_OPEN_EXISTING, &phy, &gra, &sta)) {
+		return 1;
 	}
 
 	HANDLE comPort = CreateFile(L"\\.\\COM3", GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
 	if (comPort == INVALID_HANDLE_VALUE) {
 		fprintf(stderr, "Error: open serial port \\.\\COM3\n");
-		return ERR_COM;
+		return 1;
 	}
 
 	DCB comPortDCB;
 
 	if (!GetCommState(comPort, &comPortDCB)) { //retreives  the current settings
 		fprintf(stderr, "Error: get COM state.");
-		return ERR_COM;
+		return 1;
 	}
 
 	comPortDCB.BaudRate = CBR_9600;      //BaudRate = 9600
@@ -98,7 +110,7 @@ Errorcode doSend(void)
 
 	if (!SetCommState(comPort, &comPortDCB)) {
 		fprintf(stderr, "Error: set COM state.");
-		return ERR_COM;
+		return 1;
 	}
 
 	struct SimDisplayPacket packet;
@@ -106,19 +118,18 @@ Errorcode doSend(void)
 	HANDLE sendTimer = CreateWaitableTimer(NULL, FALSE, NULL);
 	if (NULL == sendTimer) {
 		fprintf(stderr, "Error: create timer.\n");
-		return ERR_TIMER;
+		return 1;
 	}
 	LARGE_INTEGER dueTime;
 	dueTime.QuadPart = -400000LL; // 40ms == 25Hz
 	if (!SetWaitableTimer(sendTimer, &dueTime, 40, NULL, NULL, FALSE)) {
 		printf("Error: SetWaitableTimer: %d\n", GetLastError());
-		return ERR_TIMER;
+		return 1;
 	}
 	DWORD bytesWritten;
-	AC_STATUS prevStatus = AC_OFF;
+	int prevStatus = ACC_OFF;
 	while (WaitForSingleObject(sendTimer, INFINITE) == WAIT_OBJECT_0) {
-		if (AC_LIVE != gra->status && prevStatus == gra->status) continue;
-		if (prevStatus != gra->status) fprintf(stderr, "status changed: %d\n", gra->status); // TODO: remove, this is for debug only
+		if (ACC_LIVE != gra->status && prevStatus == gra->status) continue;
 		packet.status = prevStatus = gra->status;
 		packet.gear = phy->gear;
 		packet.rpms = phy->rpms;
@@ -128,8 +139,8 @@ Errorcode doSend(void)
 		packet.bb = 50;
 		packet.map = gra->EngineMap;
 		packet.remlaps = 255;// gra->fuelEstimatedLaps; // TODO: Add when redone capture
-		packet.airt = (char)lroundf(phy->airTemp);
-		packet.roadt = (char)lroundf(phy->roadTemp);
+		packet.airt = (uint8_t)lroundf(phy->airTemp);
+		packet.roadt = (uint8_t)lroundf(phy->roadTemp);
 
 		WriteFile(comPort, &packet, sizeof(packet), &bytesWritten, NULL);
 		// TODO: validate bytes written
@@ -137,37 +148,35 @@ Errorcode doSend(void)
 		// TODO: what if I disconnect and reconnect the arduino?
 	}
 	fprintf(stderr, "Error: WaitForSingleObject: %d\n", GetLastError());
-	return ERR_TIMER;
+	return 1;
 }
 
-Errorcode doDump(void)
+int doDump(void)
 {
-	Errorcode err;
-	
 	struct ACCPhysics *phy;
 	struct ACCGraphics *gra;
 	struct ACCStatic *sta;
 
-	if (err = mapAcpmf(&phy, &gra, &sta)) {
-		return err;
+	if (mapAcpmf(MAPACPMF_OPEN_EXISTING, &phy, &gra, &sta)) {
+		return 1;
 	}
 
 	fprintf(stderr, "Read memory at 50Hz and dump contents to accdump.bin\n");
 	HANDLE dumpFile = CreateFile(TEXT("accdump.bin"), GENERIC_WRITE, FILE_SHARE_READ, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
 	if (INVALID_HANDLE_VALUE == dumpFile) {
 		fprintf(stderr, "Error: create accdump.bin\n");
-		return ERR_FILE;
+		return 1;
 	}
 	HANDLE dumpTimer = CreateWaitableTimer(NULL, FALSE, NULL);
 	if (NULL == dumpTimer) {
 		fprintf(stderr, "Error: create timer.\n");
-		return ERR_TIMER;
+		return 1;
 	}
 	LARGE_INTEGER dueTime;
 	dueTime.QuadPart = -200000LL; // 20ms == 50Hz
 	if (!SetWaitableTimer(dumpTimer, &dueTime, 20, NULL, NULL, FALSE)) {
 		printf("Error: SetWaitableTimer: %d\n", GetLastError());
-		return ERR_TIMER;
+		return 1;
 	}
 	DWORD bytesWritten;
 	while (WaitForSingleObject(dumpTimer, INFINITE) == WAIT_OBJECT_0) {
@@ -176,21 +185,21 @@ Errorcode doDump(void)
 		WriteFile(dumpFile, sta, sizeof(*sta), &bytesWritten, NULL);
 	}
 	fprintf(stderr, "Error: WaitForSingleObject: %d\n", GetLastError());
-	return ERR_TIMER;
+	return 1;
 }
 
-Errorcode doCsv(void)
+int doCsv(void)
 {
 	fprintf(stderr, "Read accdump.bin contents and write into accdump.csv\n");
 	HANDLE csvFile = CreateFile(TEXT("accdump.csv"), GENERIC_WRITE, FILE_SHARE_READ, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
 	if (INVALID_HANDLE_VALUE == csvFile) {
 		fprintf(stderr, "Error: create accdump.csv: %d\n", GetLastError());
-		return ERR_FILE;
+		return 1;
 	}
 	HANDLE binFile = CreateFile(TEXT("accdump.bin"), GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 	if (INVALID_HANDLE_VALUE == binFile) {
 		fprintf(stderr, "Error: open accdump.bin: %d\n", GetLastError());
-		return ERR_FILE;
+		return 1;
 	}
 	int maxCsvRecord = 8192;
 	char *csvRecord = malloc(maxCsvRecord);
@@ -200,7 +209,7 @@ Errorcode doCsv(void)
 			snprintf(csvRecord, maxCsvRecord, "Status,PHY pid,GRA pid,STA sessions\n"),
 			&writtenBytes, NULL)) {
 		fprintf(stderr, "Error: write CSV header: %d\n", GetLastError());
-		return ERR_FILE_WRITE;
+		return 1;
 	}
 	int binBufferSize = sizeof(struct ACCPhysics) + sizeof(struct ACCGraphics) + sizeof(struct ACCStatic);
 	char *binBuffer = malloc(binBufferSize);
@@ -215,79 +224,53 @@ Errorcode doCsv(void)
 					gra->status, phy->packetId, gra->packetId, sta->numberOfSessions),
 				&writtenBytes, NULL)) {
 			fprintf(stderr, "Error: write CSV record: %d\n", GetLastError());
-			return ERR_FILE_WRITE;
+			return 1;
 		}
 	}
 	return 0;
 }
 
-Errorcode doReplay(void)
+int doReplay(void)
 {
-	fprintf(stderr, "Read accdump.bin contents and dump into shared memory at 50Hz\n");
-	HANDLE phyMap = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, sizeof(struct ACCPhysics), TEXT("Local\\acpmf_physics"));
-	if (!phyMap) {
-		fprintf(stderr, "Error create file mapping for ACCPhysics.\n");
-	}
-	HANDLE graMap = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, sizeof(struct ACCGraphics), TEXT("Local\\acpmf_graphics"));
-	if (!graMap) {
-		fprintf(stderr, "Error create file mapping for ACCGraphics.\n");
-	}
-	HANDLE staMap = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, sizeof(struct ACCStatic), TEXT("Local\\acpmf_static"));
-	if (!staMap) {
-		fprintf(stderr, "Error create file mapping for ACCStatic.\n");
-	}
-	if (!staMap || !graMap || !phyMap) {
-		return ERR_MAP;
-	}
+	struct ACCPhysics *phy;
+	struct ACCGraphics *gra;
+	struct ACCStatic *sta;
 
-	struct ACCPhysics *phy = (struct ACCPhysics *) MapViewOfFile(phyMap, FILE_MAP_WRITE, 0, 0, 0);
-	if (!phy) {
-		fprintf(stderr, "Error mapping view ACCPhysics.\n");
-	}
-	struct ACCGraphics *gra = (struct ACCGraphics *) MapViewOfFile(graMap, FILE_MAP_WRITE, 0, 0, 0);
-	if (!gra) {
-		fprintf(stderr, "Error mapping view ACCGraphics.\n");
-	}
-	struct ACCStatic *sta = (struct ACCStatic *) MapViewOfFile(staMap, FILE_MAP_WRITE, 0, 0, 0);
-	if (!sta) {
-		fprintf(stderr, "Error mapping view ACCStatic.\n");
-	}
-
-	if (!phy || !gra || !sta) {
-		return ERR_MAP;
+	if (mapAcpmf(MAPACPMF_CREATE, &phy, &gra, &sta)) {
+		return 1;
 	}
 
 	HANDLE binFile = CreateFile(TEXT("accdump.bin"), GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 	if (INVALID_HANDLE_VALUE == binFile) {
 		fprintf(stderr, "Error: open accdump.bin: %d\n", GetLastError());
-		return ERR_FILE;
+		return 1;
 	}
 
 	HANDLE dumpTimer = CreateWaitableTimer(NULL, FALSE, NULL);
 	if (NULL == dumpTimer) {
 		fprintf(stderr, "Error: create timer.\n");
-		return ERR_TIMER;
+		return 1;
 	}
 	LARGE_INTEGER dueTime;
 	dueTime.QuadPart = -200000LL; // 20ms == 50Hz
 	if (!SetWaitableTimer(dumpTimer, &dueTime, 20, NULL, NULL, FALSE)) {
 		printf("Error: SetWaitableTimer: %d\n", GetLastError());
-		return ERR_TIMER;
+		return 1;
 	}
 	while (WaitForSingleObject(dumpTimer, INFINITE) == WAIT_OBJECT_0) {
 		DWORD bytesRead;
 		if (!ReadFile(binFile, phy, sizeof(*phy), &bytesRead, NULL) || bytesRead < sizeof(*phy)) {
-			return ERR_FILE_READ;
+			return 1;
 		}
 		if (!ReadFile(binFile, gra, sizeof(*gra), &bytesRead, NULL) || bytesRead < sizeof(*gra)) {
-			return ERR_FILE_READ;
+			return 1;
 		}
 		if (!ReadFile(binFile, sta, sizeof(*sta), &bytesRead, NULL) || bytesRead < sizeof(*sta)) {
-			return ERR_FILE_READ;
+			return 1;
 		}
 	}
 	fprintf(stderr, "Error: WaitForSingleObject: %d\n", GetLastError());
-	return ERR_TIMER;
+	return 1;
 }
 
 int main(int argc, char *argv[])
