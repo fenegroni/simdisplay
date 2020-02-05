2bb2268197b898ad8b362bc9632298d8b8f9b403
Filippo Erik Negroni <f.e.negroni@gmail.com>
2020-02-05 15:05:30 +0000

    Add license header

--- Device/UnoR3/arduino-ide/simdisplay/simdisplay.ino
+++ Device/UnoR3/arduino-ide/simdisplay/simdisplay.ino
@@ -1,23 +1,40 @@
 /*
-   simdisplay -
+simdisplay - A simracing dashboard created using Arduino to show shared memory telemetry from Assetto Corsa Competizione.
 
-   The LiquidCrystal library works with all LCD displays that are compatible with the
-   Hitachi HD44780 driver. There are many of them out there, and you
-   can usually tell them by the 16-pin interface.
+Copyright (C) 2020  Filippo Erik Negroni
 
-   The circuit:
-   LCD RS pin to digital pin 7
-   LCD Enable pin to digital pin 8
-   LCD D4 pin to digital pin 9
-   LCD D5 pin to digital pin 10
-   LCD D6 pin to digital pin 11
-   LCD D7 pin to digital pin 12
-   LCD R/W pin to ground
-   LCD VSS pin to ground
-   LCD VCC pin to 5V
-   10K resistor:
-   ends to +5V and ground
-   wiper to LCD VO pin (pin 3)
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/*
+The LiquidCrystal library works with all LCD displays that are compatible with the
+Hitachi HD44780 driver. There are many of them out there, and you
+can usually tell them by the 16-pin interface.
+
+The circuit:
+LCD RS pin to digital pin 7
+LCD Enable pin to digital pin 8
+LCD D4 pin to digital pin 9
+LCD D5 pin to digital pin 10
+LCD D6 pin to digital pin 11
+LCD D7 pin to digital pin 12
+LCD R/W pin to ground
+LCD VSS pin to ground
+LCD VCC pin to 5V
+10K resistor:
+ends to +5V and ground
+wiper to LCD VO pin (pin 3)
 */
 
 #include <inttypes.h>
