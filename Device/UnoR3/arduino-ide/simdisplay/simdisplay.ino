38aafbdcd818a127444b644405783db8c6d043d9
Filippo Negroni <f.e.negroni@gmail.com>
2020-02-21 08:54:42 +0000

    Refactor printRedline to use a SN74HC595 8bit shift register
    We use the Shift Register Clear pin to set all LEDs to off quickly and reset
    the clock timing.
    
    As more are added, we can use the Output Enable pin to switch all leds
    on and off using pitlimiter or to simulate ignition on and off

--- Device/UnoR3/arduino-ide/simdisplay/simdisplay.ino
+++ Device/UnoR3/arduino-ide/simdisplay/simdisplay.ino
@@ -1,21 +1,21 @@
 /*
-simdisplay - A simracing dashboard created using Arduino to show shared memory
+  simdisplay - A simracing dashboard created using Arduino to show shared memory
              telemetry from Assetto Corsa Competizione.
 
-Copyright (C) 2020  Filippo Erik Negroni
+  Copyright (C) 2020  Filippo Erik Negroni
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 #include <inttypes.h>
@@ -25,8 +25,8 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include "SimDisplayProtocol.h"
 
 // ----------------------  0123456789012345
-#define DISPLAY_MASK_ROW0 "-   --.-%   -/- "
-#define DISPLAY_MASK_ROW1 "R--  M-   --/--C"
+static char DISPLAY_MASK_ROW0[] = "-   --.-%   -/- ";
+static char DISPLAY_MASK_ROW1[] = "R--  M-   --/--C";
 
 #define DISPLAY_ABS_COLROW      0,  0
 #define DISPLAY_REMLAPS_COLROW  1,  1
@@ -38,14 +38,10 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #define DISPLAY_ROADT_COLROW    13, 1
 
 enum {
-  RL_LED1_PIN = 2,
-  RL_LED2_PIN = 3,
-  RL_LED3_PIN = 4,
-  RL_LED4_PIN = 5,
-  RL_LED5_PIN = 6,
-  RL_LED6_PIN = 7,
-  RL_LED7_PIN = 8,
-  RL_LED8_PIN = 9
+  RL_CLOCK_PIN = 2,
+  RL_LATCH_PIN = 3,
+  RL_DATA_PIN = 4,
+  RL_CLEAR_PIN = 5
 };
 
 enum {
@@ -58,23 +54,23 @@ enum {
 };
 
 /*
-The LiquidCrystal library works with all LCD displays that are compatible with the
-Hitachi HD44780 driver. There are many of them out there, and you
-can usually tell them by the 16-pin interface.
+  The LiquidCrystal library works with all LCD displays that are compatible with the
+  Hitachi HD44780 driver. There are many of them out there, and you
+  can usually tell them by the 16-pin interface.
 
-The circuit:
-LCD RS pin to digital pin 10
-LCD Enable pin to digital pin 11
-LCD D4 pin to digital pin 14 (A0)
-LCD D5 pin to digital pin 15 (A1)
-LCD D6 pin to digital pin 16 (A2)
-LCD D7 pin to digital pin 17 (A3)
-LCD R/W pin to ground
-LCD VSS pin to ground
-LCD VDD pin to 5V
-10K resistor:
-ends to +5V and ground
-wiper to LCD VO pin (pin 3)
+  The circuit:
+  LCD RS pin to digital pin 10
+  LCD Enable pin to digital pin 11
+  LCD D4 pin to digital pin 14 (A0)
+  LCD D5 pin to digital pin 15 (A1)
+  LCD D6 pin to digital pin 16 (A2)
+  LCD D7 pin to digital pin 17 (A3)
+  LCD R/W pin to ground
+  LCD VSS pin to ground
+  LCD VDD pin to 5V
+  10K resistor:
+  ends to +5V and ground
+  wiper to LCD VO pin (pin 3)
 */
 LiquidCrystal lcd(LCD_RS_PIN, LCD_ENABLE_PIN, LCD_D4_PIN, LCD_D5_PIN, LCD_D6_PIN, LCD_D7_PIN);
 
@@ -94,10 +90,10 @@ static void printDisplayMask()
   lcdPrint(DISPLAY_MASK_ROW1, 0, 1);
 }
 
-static void printDisplayField(int newval, int oldval, char *zerostr, char *fmtstr, int col, int row)
+static void printDisplayField(int newval, int oldval, const char *zerostr, const char *fmtstr, int col, int row)
 {
   static char strbuffer[17];
-  
+
   if (newval != oldval) {
     if (0 == newval) {
       strcpy(strbuffer, zerostr);
@@ -111,12 +107,12 @@ static void printDisplayField(int newval, int oldval, char *zerostr, char *fmtst
 static void printDisplayFieldBB(int newval, int oldval, int col, int row)
 {
   static char strbuffer[17];
-  
+
   if (newval != oldval) {
     if (0 == newval) {
       strcpy(strbuffer, "--.-");
     } else {
-      sprintf(strbuffer, "%d.%d", newval/10, newval%10);
+      sprintf(strbuffer, "%d.%d", newval / 10, newval % 10);
     }
     lcdPrint(strbuffer, col, row);
   }
@@ -124,10 +120,8 @@ static void printDisplayFieldBB(int newval, int oldval, int col, int row)
 
 static void printDisplayFields()
 {
-  static char strbuffer[17];
-  
   printDisplayField(newPacket->abs, oldPacket->abs, "- ", "%-2d", DISPLAY_ABS_COLROW);
-  printDisplayField(newPacket->remlaps, oldPacket->remlaps, "--", "%2d", DISPLAY_REMLAPS_COLROW); 
+  printDisplayField(newPacket->remlaps, oldPacket->remlaps, "--", "%2d", DISPLAY_REMLAPS_COLROW);
   printDisplayField(newPacket->tc, oldPacket->tc, " -", "%2d", DISPLAY_TC_COLROW);
   printDisplayField(newPacket->tcc, oldPacket->tcc, "- ", "%-2d", DISPLAY_TCC_COLROW);
   printDisplayFieldBB(newPacket->bb, oldPacket->bb, DISPLAY_BB_COLROW);
@@ -136,37 +130,58 @@ static void printDisplayFields()
   printDisplayField(newPacket->roadt, oldPacket->roadt, "--", "%2d", DISPLAY_ROADT_COLROW);
 }
 
-static int rlledpins[] = {RL_LED1_PIN, RL_LED2_PIN, RL_LED3_PIN, RL_LED4_PIN, RL_LED5_PIN, RL_LED6_PIN, RL_LED7_PIN, RL_LED8_PIN };
+void writeRedline(uint8_t pattern)
+{
+  digitalWrite(RL_LATCH_PIN, LOW);
+  shiftOut(RL_DATA_PIN, RL_CLOCK_PIN, LSBFIRST, pattern);
+  digitalWrite(RL_LATCH_PIN, HIGH);
+}
+
+static void clearRedline()
+{
+  digitalWrite(RL_LATCH_PIN, LOW);
+  digitalWrite(RL_CLEAR_PIN, LOW);
+  digitalWrite(RL_LATCH_PIN, HIGH);
+  digitalWrite(RL_CLEAR_PIN, HIGH);
+}
 
 static void printRedline()
 {
   static unsigned long bktm = 0;
-  static int bksta = LOW;
-  static unsigned long bkint = 160;
-  
+  static uint8_t bksta = B00000000;
+  static const unsigned long bkint = 100;
+
   if (newPacket->rpm > newPacket->shftrpm) {
     if (bktm > 0 && millis() - bktm < bkint) {
-        return;
+      return;
     }
-    bksta = !bksta; // HIGH and LOW are just 1 and 0!
-    for (int led = 0; led < 8; ++led) {
-      digitalWrite(rlledpins[led], bksta);
-    }
+    writeRedline(bksta = ~bksta);
     bktm = millis();
-  } else {
-    bktm = 0;
-    bksta = LOW;
-    uint16_t steprpm = (newPacket->shftrpm - newPacket->optrpm) / 8;
-    uint16_t ledrpm = newPacket->optrpm;
-    for (int led = 0; led < 8; ++led) {
-      if (newPacket->rpm > ledrpm) {
-        if (!digitalRead(rlledpins[led])) digitalWrite(rlledpins[led], HIGH);
-      } else {
-        if (digitalRead(rlledpins[led])) digitalWrite(rlledpins[led], LOW);
-      }
-      ledrpm += steprpm;
-    }
+    return;
   }
+  
+  bktm = 0;
+  bksta = B00000000;
+  
+  if (newPacket->rpm > newPacket->optrpm) {
+    static const int RLSTAGES = 8;
+    static uint8_t patterns[RLSTAGES] = {
+      B00000001,
+      B00000011,
+      B00000111,
+      B00001111,
+      B00011111,
+      B00111111,
+      B01111111,
+      B11111111,
+    };
+  
+    int stage = (newPacket->rpm - newPacket->optrpm) / ((newPacket->shftrpm - newPacket->optrpm) / RLSTAGES);
+    writeRedline(patterns[stage]);
+    return;
+  }
+  
+  clearRedline();
 }
 
 void setup()
@@ -175,29 +190,33 @@ void setup()
   lcd.begin(16, 2);
   printDisplayMask();
 
-  for (int led = 0; led < 8; ++led) {
-    pinMode(rlledpins[led], OUTPUT);
-  }
-  
+  pinMode(RL_LATCH_PIN, OUTPUT);
+  pinMode(RL_CLOCK_PIN, OUTPUT);
+  pinMode(RL_DATA_PIN, OUTPUT);
+  pinMode(RL_CLEAR_PIN, OUTPUT);
+  clearRedline();
+
   Serial.begin(9600);
 }
 
 void loop()
 {
-  while (Serial.available() >= sizeof(struct SimDisplayPacket)) {
+  static const int packetsize = sizeof(struct SimDisplayPacket);
+  while (Serial.available() >= packetsize) {
     unsigned long time = micros();
-    
-    if (sizeof(struct SimDisplayPacket) > Serial.readBytes((byte *)newPacket, sizeof(struct SimDisplayPacket))) {
+
+    if (packetsize > Serial.readBytes((byte *)newPacket, packetsize)) {
       // something went wrong, we reset ourselves.
       Serial.end();
       delay(2000);
       printDisplayMask();
+      clearRedline();
       Serial.begin(9600);
       break;
     }
     if (SDP_STATUS_LIVE != newPacket->status && SDP_STATUS_PAUSE != newPacket->status) {
       printDisplayMask();
-      printRedline();
+      clearRedline();
       continue;
     }
     printRedline();
@@ -205,7 +224,7 @@ void loop()
     struct SimDisplayPacket *tmp = oldPacket;
     oldPacket = newPacket;
     newPacket = tmp;
-    
+
     time = micros() - time;
 
     if (time > 20000) {
